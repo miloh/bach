08c0a700e62a75419365f472465b6793f36f4be7
R. Miloh Alexander <miloh@typeamachines.com>
2014-10-21 11:32:41 -0700

    add demoscript to repo

---
+++ demoscript.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+#
+# script for setting up gui keypress capture/notification and screen capture for the EDA demo at the BACH unconference, 2014
+#
+set -x
+# use key
+key-mon --old-keys 1 -l
+#screenkey 
+
